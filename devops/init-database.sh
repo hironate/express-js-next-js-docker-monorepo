2d165a8d6ffa2d9dd9b4c306cf7d522c8c23766a
hironate <hirenkavad@gmail.com>
2023-06-20 22:37:53 +0530

    db permissions updated

--- devops/init-database.sh
+++ devops/init-database.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-CREATE USER monorepo;
-ALTER USER monorepo WITH PASSWORD 'monorepo';
-GRANT ALL ON SCHEMA public TO monorepo;
+CREATE USER monorepo_user;
+ALTER USER monorepo_user WITH PASSWORD 'monorepo-password';
+GRANT ALL ON DATABASE "$POSTGRES_DB" TO monorepo_user;
+GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO monorepo_user;
+GRANT ALL PRIVILEGES ON SCHEMA public TO monorepo_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO monorepo_user;
 EOSQL
